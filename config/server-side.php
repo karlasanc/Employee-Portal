8727a004d4aa7c3c4684d9b121c864624d2cf6df
Karla Sanchez <ksanch7@gmail.com>
2023-03-21 18:32:31 -0400

    TASK 1 COMPLETE

---
+++ config/server-side.php
@@ -0,0 +1,13 @@
+<?php
+  #$_SERVER SUPERGLOBAL
+
+  // Create Server Array -> defines key and value
+  $server = [
+    'Host Server Name' => $_SERVER['SERVER_NAME'];
+
+  ];
+  echo $server['Host Server Name'];
+  
+  // Create Client Array
+
+?>
